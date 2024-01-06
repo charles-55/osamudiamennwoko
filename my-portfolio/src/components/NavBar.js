f57a519aaccb4cfbc84213e33832881d5288f90c
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2024-01-06 13:26:46 -0700

    banner update

--- my-portfolio/src/components/NavBar.js
+++ my-portfolio/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { NavBar, Container } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
@@ -43,7 +43,7 @@ export const NavBar = () => {
                     <Nav.Link href="#projects" className={activeLink === "projects" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("projects")}>Projects</Nav.Link>
                 </Nav>
                 <span className="navbar-text">
-                    <div className="social-icons">
+                    <div className="social-icon">
                         <a href="#"><img src={navIcon1} alt="" /></a>
                         <a href="#"><img src={navIcon2} alt="" /></a>
                         <a href="#"><img src={navIcon3} alt="" /></a>
