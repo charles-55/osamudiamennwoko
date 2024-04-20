4da081891f31686e38757dc969354a4092542e8a
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2024-04-20 19:05:41 -0400

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,34 +20,32 @@ export const Projects = () => {
                             }
                         </TrackVisibility>
                         <TrackVisibility>
-                            {({ isVisible }) =>
-                                <div className={isVisible ? "animate__animated animate__slideInRight" : "hidden"}>
-                                    <Tab.Container id="projects-tabs" defaultActiveKey="tab1">
-                                        <Nav variant="pills" className='nav-pills mb-5 justify-content-center align-items-center' id='pills-tab'>
-                                            {
-                                                projects.map((proj, index) => (
-                                                    <Nav.Item>
-                                                        <Nav.Link eventKey={"tab" + index} >{"Tab " + (index+1)}</Nav.Link>
-                                                    </Nav.Item>
-                                                ))
-                                            }
-                                        </Nav>
-                                        <Tab.Content>
-                                            {
-                                                projects.map((proj, index) => (
-                                                    <Tab.Pane eventKey={"tab" + index}>
-                                                        <Row>
-                                                            <ProjectCard
-                                                                {...proj}
-                                                            />
-                                                        </Row>
-                                                    </Tab.Pane>
-                                                ))
-                                            }
-                                        </Tab.Content>
-                                    </Tab.Container>
-                                </div>
-                            }
+                            <div className="animate__animated animate__slideInRight">
+                                <Tab.Container id="projects-tabs" defaultActiveKey="tab1">
+                                    <Nav variant="pills" className='nav-pills mb-5 justify-content-center align-items-center' id='pills-tab'>
+                                        {
+                                            projects.map((proj, index) => (
+                                                <Nav.Item>
+                                                    <Nav.Link eventKey={"tab" + index} >{"Tab " + (index+1)}</Nav.Link>
+                                                </Nav.Item>
+                                            ))
+                                        }
+                                    </Nav>
+                                    <Tab.Content>
+                                        {
+                                            projects.map((proj, index) => (
+                                                <Tab.Pane eventKey={"tab" + index}>
+                                                    <Row>
+                                                        <ProjectCard
+                                                            {...proj}
+                                                        />
+                                                    </Row>
+                                                </Tab.Pane>
+                                            ))
+                                        }
+                                    </Tab.Content>
+                                </Tab.Container>
+                            </div>
                         </TrackVisibility>
                     </Col>
                 </Row>
