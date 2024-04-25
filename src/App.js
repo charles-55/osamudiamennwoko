1b32a8a6d81377009b89bc7f19d6594c135a6852
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2024-04-25 14:30:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 	return (
 		<div className="App">
-		<NavBar />
-		<Banner />
-		<Skills />
-		<Projects />
-		<Contact />
-		<Footer />
+			<NavBar />
+			<Banner />
+			<Skills />
+			<Projects />
+			<Contact />
+			<Footer />
 		</div>
 	);
 }
