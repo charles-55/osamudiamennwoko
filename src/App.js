c29297597ebf50995b5bc877967a796f132df3db
Osamudiamen Nwoko <59775183+charles-55@users.noreply.github.com>
2024-04-25 10:04:13 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import { useEffect } from 'react';
 import { NavBar, Banner, Skills, Projects, Contact, Footer } from './components';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-    
-    useEffect(() => {
-        document.title = 'Osamudiamen Nwoko';
-    }, []);
-    
 	return (
 		<div className="App">
 		<NavBar />
